f931ee1471f818ef61d0be4a16b27b1549eaf784
jhervera1 <herverajoel@gmail.com>
2021-10-18 21:51:32 +0200

    ejercicios2 parte 1

--- app/Heroes/listado/listado.component.ts
+++ app/Heroes/listado/listado.component.ts
@@ -5,14 +5,34 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './listado.component.html',
   styleUrls: ['./listado.component.css']
 })
+
 export class ListadoComponent implements OnInit {
 
   heroes:string[] = ["Spoderman","Orin man", "Thortilla","Hush"];
-
+  heroeBorrado:string = "";
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  borrarHeroe(heroe:string){
+
+    this.heroes.forEach((element, index) =>{
+      if(element === heroe){
+        this.heroes.splice(index,1);
+      }
+    })
+  }
+  borrarPrimerUltimo(){
+    let firstHero : string;
+    let lastHero: string;
+
+    firstHero = this.heroes[0];
+    //lastHero = this.heroes[this.heroes.length];
+    this.heroes.splice(0,1);
+    //this.heroes.splice(this.heroes.length,1);
+    this.heroeBorrado = firstHero;
   }
 
 }
